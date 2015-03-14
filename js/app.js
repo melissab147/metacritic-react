9fdef9d05354056f790c11d5a4943db816337783
melissab147 <melissa.j.bica@gmail.com>
2015-03-14 17:34:23 -0600

    build

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
-$( "#doctors" ).on( "click", function( event ) {
+$( "#movies" ).on( "click", function( event ) {
     React.render(
-        <DoctorList url='data/doctors.json.data'/>,
+        <MovieList url='data/movies.json.data'/>,
         document.getElementById('content')
     )
 })
 
-$( "#users" ).on( "click", function( event ) {
+$( "#music" ).on( "click", function( event ) {
     React.render(
-        <UserList url='data/users.json.data'/>,
+        <MusicList url='data/music.json.data'/>,
         document.getElementById('content')
     )
 })
 
-$( "#restaurants" ).on( "click", function( event ) {
-    React.render(
-        <RestaurantList url='data/restaurants.json.data'/>,
-        document.getElementById('content')
-    )
-})
-
+unirest.get("https://byroredux-metacritic.p.mashape.com/album-list/new-releases/date")
+.header("X-Mashape-Key", "O2nZPA6e8Hmsh3RkfRfRUdfAxyv0p10Ad25jsnCBE8lxPqvdeb")
+.header("Accept", "application/json")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
