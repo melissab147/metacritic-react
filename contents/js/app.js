4bca4bea59c2e293a784df3f3ead690ea4484784
melissab147 <melissa.j.bica@gmail.com>
2015-03-14 14:15:09 -0600

    removed yelp files

--- contents/js/app.js
+++ contents/js/app.js
@@ -1,24 +1,3 @@
-$( "#doctors" ).on( "click", function( event ) {
-    React.render(
-        <DoctorList url='data/doctors.json.data'/>,
-        document.getElementById('content')
-    )
-})
-
-$( "#users" ).on( "click", function( event ) {
-    React.render(
-        <UserList url='data/users.json.data'/>,
-        document.getElementById('content')
-    )
-})
-
-$( "#restaurants" ).on( "click", function( event ) {
-    React.render(
-        <RestaurantList url='data/restaurants.json.data'/>,
-        document.getElementById('content')
-    )
-})
-
 $( "#movies" ).on( "click", function( event ) {
     React.render(
         <MovieList url='data/movies.json.data'/>,
